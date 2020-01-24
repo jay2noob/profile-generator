ed688a81044d974fd8573c10cec5807707a39e21
John Fields <jjfields24@outlook.com>
2020-01-24 01:32:33 -0500

    apis

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ function promptUser() {
 }
 
 function generateHTML(answers) {
-  const queryUrl = `https://api.github.com/users/${answers.name}`;
+  const user = `https://api.github.com/users/${answers.name}`;
+  const repo = `https://api.github.com/users/${answers.name}/repo`;
+  
   
   const resume = `
 <!DOCTYPE html>
@@ -34,8 +36,47 @@ function generateHTML(answers) {
   <title>Document</title>
 </head>
 <body>
-  <div>${answers.name}</div>
-  <div>${answers.color}</div>
+<div class="card border-primary mb-3" style="max-width: 100rem;">
+<div class="card-header"><h3>${user.name}</h3></div>
+<div class="card-body">
+  <div class="row">
+  <div class="col-md-3">
+    <img class="img-thumbnail avatar" src="${user.avatar_url}">
+    <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+  </div>
+  <div class="col-md-9">
+    <span class="badge badge-dark">Public Repos: ${user.public_repos}</span>
+    <span class="badge badge-primary">Public Gists: ${user.public_gists}</span>
+    <span class="badge badge-success">Followers: ${user.followers}</span>
+    <span class="badge badge-info">Following: ${user.following}</span>
+    <br><br>
+    <ul class="list-group">
+      <li class="list-group-item">Company: ${user.company}</li>
+      <li class="list-group-item">Website/blog: <a href="${user.blog}" target="_blank">${user.blog}</a></li>
+      <li class="list-group-item">Location: ${user.location}</li>
+      <li class="list-group-item">Member Since: ${user.created_at}</li>
+    </ul>
+    </div>
+  </div>
+</div>
+</div>
+<div class="card">
+                <div class="row">
+                  <div class="col-md-7">
+                    <strong>${repo.name}</strong>: ${repo.description}
+                  </div>
+                  <div class="col-md-3">
+                    <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                    <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
+                    <span class="badge badge-success">Stars: ${repo.stargazers_count}</span>
+                  </div>
+                  <div class="col-md-2">
+                    <a href="${repo.html_url}" target="_blank" class="btn btn-dark">Repo Page</a>
+                  </div>
+                </div>
+              </div>
+<h3 class="page-header">Latest Repos</h3>
+<div id="repos"></div>
 </body>
 </html>`;
 fs.writeFile(`./html/${answers.name}.html`, resume, function (err) {
@@ -62,11 +103,7 @@ promptUser()
   .then(function(answers) {
     const html = generateHTML(answers);
 
-    return writeFileAsync("index.html", html);
-
-    
-    
-  
+    //return writeFileAsync("index.html", html);
   })
   .then(function() {
     console.log("Successfully wrote to index.html");
